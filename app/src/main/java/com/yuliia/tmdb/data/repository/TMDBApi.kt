13c0a35c9cd3a87eda72ad780b863a3112aed23d
Yuliia Stelmakhovska <ystelmahovskaya@gmail.com>
2026-02-17 19:57:32 +0100

    Add Result class

--- app/src/main/java/com/yuliia/tmdb/data/repository/TMDBApi.kt
+++ app/src/main/java/com/yuliia/tmdb/data/repository/TMDBApi.kt
@@ -1,5 +1,6 @@
 package com.yuliia.tmdb.data.repository
 
+import com.yuliia.tmdb.data.remote.dto.MovieListResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -7,5 +8,5 @@ import retrofit2.http.Path
 interface TMDBApi {
 
     @GET("trending/movie/{time_window}")
-    suspend fun getTrendingMovies(@Path ("time_window") timeWindow: String)
+    suspend fun getTrendingMovies(@Path ("time_window") timeWindow: String): MovieListResponse
 }
---
+++ app/src/main/java/com/yuliia/tmdb/util/Result.kt
@@ -0,0 +1,8 @@
+package com.yuliia.tmdb.util
+
+
+sealed class Result<out T> {
+    data class Success<T>(val data: T) : Result<T>()
+    data class Error(val exception: Throwable, val message: String? = null) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+}
